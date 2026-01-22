b8520e1658a13e5961a7b69448a1571fe3ad7b26
Suyash Labde <suyashlabde123@gmail.com>
2026-01-22 19:46:24 +0530

    refactor : minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "../components/providers/ThemeProvider";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -38,9 +38,9 @@ export default function RootLayout({
         <link rel="apple-touch-icon" href="/icon-192.png" />
       </head>
       <body className={inter.className}>
-        <ThemeProvider>
+       
           {children}
-        </ThemeProvider>
+        
       </body>
     </html>
   );
